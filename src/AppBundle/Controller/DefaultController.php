275732c0317177fda41eb0d3f4c249fd1f2c99fa
igogosha <igogosha@igogosha-VirtualBox.(none)>
2015-04-16 00:00:50 +0300

    embed code

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,5 +15,17 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/getTeasers/{id}", name="getTeasers")
+     */
+    public function createTeaserBlockAction($id)
+    {
+        $response = new Response($this->renderView('AppBundle:Default:teaserBlock.html.twig', array(
+            'id' => $id
+        )));
+        $response->headers->set('Content-Type', 'application/javascript');
+        return $response;
     }
 }
